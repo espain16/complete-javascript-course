f8f03e07b01ecffed9fd281430d2daa0481b9b05
Elise Spain <elisespain@gmail.com>
2020-05-01 10:20:38 -0700

    Dice rolling event listener setup

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,20 +8,33 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0; 
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
 
 
-document.querySelector('#current-' + activePlayer).textContent = dice; 
+
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  
+  //1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+  
+  //2. Display the result 
+  var diceDOM = document.querySelector('.dice')
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png'
+  
+  
+  //3. Update the round score IF the rolled number was NOT a 1 
+  
+  
+  
+}); 
+//document.querySelector('#current-' + activePlayer).textContent = dice; 
 //textContent is used to change the text
-
-document.querySelector('.dice').style.display = 'none';
-
-
-
-document.querySelector('.btn-roll').addEventListener('click', )
