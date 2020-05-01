ed5515538af2f8c53a3eead73935c69389d1fade
Elise Spain <elisespain@gmail.com>
2020-05-01 12:59:36 -0700

    Hold button and general styling applied

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -4,7 +4,7 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
@@ -23,8 +23,6 @@ document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
-
-
 document.querySelector('.btn-roll').addEventListener('click', function() {
   
   //1. Random number
@@ -42,23 +40,59 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
     //Next player 
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    //Reset the score once a player rolls a 1 
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    //Toggling the active class on and off based on players (used add and remove initially)
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-    document.querySelector('.dice').style.display = 'none';
-
-    
+    nextPlayer();
 
   }
-  
-  
+    
 }); 
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  
+  //Add current score to global score 
+  scores[activePlayer] += roundScore;
+  // Update the UI 
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+  // Check if player won the game 
+  if(scores[activePlayer] >= 20) {
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+  } else {
+    
+    nextPlayer();
+  }
+  //Next Player
+});
+
+
+function nextPlayer() {
+  //Next player 
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  //Reset the score once a player rolls a 1 
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  //Toggling the active class on and off based on players (used add and remove initially)
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  document.querySelector('.dice').style.display = 'none';
+
+}
+
+
+
+
+
+
+
+
+
+
+
 //document.querySelector('#current-' + activePlayer).textContent = dice; 
 //textContent is used to change the text
